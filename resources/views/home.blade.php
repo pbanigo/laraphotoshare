8ed06067fb1073e010365758ddf544f7cd84eaf1
pbanigo <peter@targetict.co.uk>
2019-10-13 22:12:01 +0100

    Expanded Logo. Edits to Dashboard.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,16 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
+    
+<div class="row">
+    <div class="col-4 p-5"><img src="https://targetict.co.uk/assets/images/logo.jpg" class="rounded-circle"></div>
+    <div class="col-8">
+        <div>
+            <h1>LaraPhotoShare</h1>
         </div>
     </div>
+</div>
+
+
 </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,9 +23,9 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    <div><img src="/svg/logo.svg" style="max-height: 25px"></div>
-                    <div>Laraphotoshare</div>
+                <a class="navbar-brand d-flex" href="{{ url('/') }}">
+                    <div><img src="/svg/logo.svg" style="height: 32px; border-right: 2px solid #000" class="pr-3"></div>
+                    <div class="pl-3 pt-1">Laraphotoshare</div>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
