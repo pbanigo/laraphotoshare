bd626c3bf3595c6ac7643c2b95086663e5660689
pbanigo <peter@targetict.co.uk>
2019-10-13 21:46:19 +0100

    Logo and Favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,8 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    Laraphotoshare
+                    <div><img src="/svg/logo.svg" style="max-height: 25px"></div>
+                    <div>Laraphotoshare</div>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
