5e8b4f9f7541692d1da8e2b0725fb04250da70bd
Peter <peter@targetict.co.uk>
2019-10-14 12:22:09 +0100

    Back on

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,8 +6,11 @@
 <div class="row">
     <div class="col-4 p-5"><img src="https://targetict.co.uk/assets/images/logo.jpg" class="rounded-circle"></div>
     <div class="col-8">
-        <div>
-            <h1>LaraPhotoShare</h1>
+        <div class="pt-5"><h1>laraphotoshare</h1></div>
+        <div class="d-flex">
+        	<div class="pr-5"><strong>59</strong> posts</div>
+        	<div class="pr-5"><strong>11k</strong> followers</div>
+        	<div class="pr-5"><strong>900</strong> following</div>
         </div>
     </div>
 </div>
